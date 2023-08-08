85305b6ad9839571189882d76e1b212301b0a0ae
Goddzy <renatotrevisiolmontiel@gmail.com>
2023-08-08 10:43:24 -0300

    cuando se agrega un producto, la tabla lo dibuja

--- js/administrador.js
+++ js/administrador.js
@@ -7,7 +7,7 @@ let imagen = document.getElementById('imagen');
 let descripcion = document.getElementById('descripcion');
 let stock = document.getElementById('stock');
 let formulario = document.getElementById('formularioProducto');
-let listaProductos = [];
+let listaProductos = JSON.parse(localStorage.getItem('listaProductosKey')) || [];
 
 const modalProductos = new bootstrap.Modal(
     document.getElementById("ventanaModal")
@@ -26,7 +26,52 @@ function crearProducto(e){
     e.preventDefault();
     let nuevoProducto = new Producto(codigo.value, nombre.value, precio.value, categoria.value, imagen.value, descripcion.value, stock.value);
     listaProductos.push(nuevoProducto);
+    guardarProductosEnLocalStorage();
+    crearFila(nuevoProducto);
     resetearFormulario();
+}
+
+cargarProductos();
+function cargarProductos(){
+    if(listaProductos.length >0){
+        listaProductos.forEach((productos)=>{crearFila(productos);
+        })
+    }
+}
+
+function crearFila(producto){
+    let tablaBody = document.getElementById('tablaBody');
+    tablaBody.innerHTML += `<tr>
+    <td>
+        ${producto.codigo}
+    </td>
+    <td>${producto.nombre}</td>
+    <td>
+     ${producto.precio}
+    </td>
+    <td>
+       ${producto.categoria}
+    </td>
+    <td>
+     ${producto.imagen}
+    </td>
+    <td>
+        ${producto.descripcion}
+    </td>
+    <td>
+     ${producto.stock}
+    </td>
+    <td>
+        <ul>
+            <button class="btn btn-danger my-1" type="button">
+                <i class="fa-solid fa-eraser"></i>
+            </button>
+            <button class="btn btn-warning my-1" type="button">
+                <i class="fa-regular fa-pen-to-square"></i>
+            </button>
+        </ul>
+    </td>
+</tr>`
 }
 
 
@@ -39,4 +84,8 @@ function reiniciarForm(){
     let validaciones = document.getElementsByClassName('form-control');
     const campoValidaciones = Array.from(validaciones);
     campoValidaciones.forEach((validaciones)=>{validaciones.className= 'form-control'})
+}
+
+function guardarProductosEnLocalStorage(){
+    localStorage.setItem('listaProductosKey' , JSON.stringify(listaProductos));
 }
